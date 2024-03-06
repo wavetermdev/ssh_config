17e2087ebde2d798887e9ab1ca5221dcb64a992a
Sylvie Crowe <107814465+oneirocosm@users.noreply.github.com>
2024-03-05 20:10:34 -0800

    Update IdentityFile Defaults to Match Documentation (#4)
    Credit to @virtuald for the original implementation of this fix.
    
    * Add support for retrieving all IdentityFile directives via DefaultAll
    
    * fix: set IdentityFile defaults to match man page
    
    The existing default IdentityFile list was incomplete and out of order.
    This updates it to match the defaults listed here:
    https://man7.org/linux/man-pages/man5/ssh_config.5.html
    
    ---------
    
    Co-authored-by: Dustin Spicuzza <dustin@virtualroadside.com>

--- validators.go
+++ validators.go
@@ -15,6 +15,26 @@ func Default(keyword string) string {
 	return defaults[strings.ToLower(keyword)]
 }
 
+// DefaultAll returns the default value for the given keyword, but as a slice. If
+// there is no default for the keyword, nil is returned.
+//
+// Some multi-valued settings have different defaults based on other settings, so
+// you must provide the host alias and a config to retrieve a setting from
+func DefaultAll(keyword string, alias string, cfg config) []string {
+	if strings.ToLower(keyword) == "identityfile" && cfg.getinternal(alias, "Protocol") == "2" {
+		def := make([]string, len(defaultProtocol2Identities))
+		copy(def, defaultProtocol2Identities)
+		return def
+	}
+
+	def := Default(keyword)
+	if def != "" {
+		return []string{def}
+	}
+
+	return nil
+}
+
 // Arguments where the value must be "yes" or "no" and *only* yes or no.
 var yesnos = map[string]bool{
 	strings.ToLower("BatchMode"):                        true,
@@ -163,10 +183,12 @@ var defaults = map[string]string{
 
 // these identities are used for SSH protocol 2
 var defaultProtocol2Identities = []string{
-	"~/.ssh/id_dsa",
-	"~/.ssh/id_ecdsa",
-	"~/.ssh/id_ed25519",
 	"~/.ssh/id_rsa",
+	"~/.ssh/id_ecdsa",
+	"~/.ssh/id_ecdsa_sk",
+	"~/.ssh/id_ed25519",
+	"~/.ssh/id_ed25519_sk",
+	"~/.ssh/id_dsa",
 }
 
 // these directives support multiple items that can be collected
